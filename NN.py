ccfb27dd7529f1e8ac009bd6a1dc28fb0161d0a8
Marc Harary <marcharary@Marcs-MacBook-Air.local>
2020-09-24 16:09:11 -0400

    Work on backprop and forwardprop

--- NN.py
+++ NN.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# http://neuralnetworksanddeeplearning.com/chap2.html
+
 class NN:
     """ implements fully-connected feed-forward neural network """
 
@@ -7,8 +9,11 @@ class NN:
         assert(len(layers) >= 2)
         assert(all(map(lambda x: x == "ReLU" or x == "sigmoid",
                        activations)))
+        assert(len(layers) == len(activations))
 
         self.activations = activations
+        self.L = len(layers)
+
         # zero initialization for bias vectors
         self.bs = [np.zeros((layer,1)) for layer in layers]
         # Xavier initialization for weight vectors
@@ -53,7 +58,7 @@ class NN:
         """ propagates tensor forwards through network """
         assert(X.shape[0] == (self.Ws[0].shape[0]))
         self.Zs = []
-        self.As = []
+        self.As = [X]
 
         # calculate activation for first layer
         Z1 = self.Ws[0].T @ X
@@ -66,37 +71,35 @@ class NN:
         
         # calculate activations for subsequent layers
         for i in range(1, len(self.Ws)):
-            curW = self.Ws[i]
-            preA = self.As[i-1]
-            curZ = curW.T @ preA
-
+            Z = self.Ws[i].T @ self.As[i] + self.bs[i]
             if self.activations[i] == "ReLU":
-                curA = self.ReLU(curZ)
+                A = self.ReLU(Z)
             else:
-                curA = self.sigmoid(curZ)
-
-            self.Zs.append(curZ)
-            self.As.append(curA)
+                A = self.sigmoid(Z)
+            self.Zs.append(Z)
+            self.As.append(A)
 
     
     def back_propagate(self, Y):
         """ gets gradients for each layers given forward propagation
             has already occurred """
         m = self.Ws[0].shape[1]
+        
+        self.deltas = self.dAs = self.dZs = self.dWs =\
+            self.dbs = self.L * [None]
+    
         # calculate backprop for last layer
-        self.dZs = [ self.As[-1] - Y ]
-        self.dWs = [ self.dZs[0] @ self.As[-2].T ]
-        self.dbs = (1/m) * np.sum(self.dZs[0], axis=1, keepdims=True)
+        self.dAs[self.L-1] = self.As[self.L-1] - Y
+        if self.activations[self.L-1] == "ReLU":
+            self.deltas[self.L-1] = self.dAs[self.L-1] *\
+                self.ReLUPrime(self.Zs[self.L-1])
+        else:
+            self.deltas[self.L-1] = self.dAs[self.L-1] *\
+                self.sigmoidPrime(self.Zs[self.L-1])
 
         # calculate backprop for subsequent layers
-        for i in range(1, len(self.Ws)):
-            preW = self.Ws[-i]
-            preZ = self.Zs[-i]
-            preDZ = self.dZs[-i]
-            curZ = self.Zs[-(i+1)]
-            if self.activations[-(i+1)] == "ReLU":
-                curDZ = preW.T @ preDZ * self.ReLUPrime(curZ)
-            else:
-                curDZ = preW.T @ preDZ * self.sigmoidPrime(curZ)
-            curDw = (1/m)*curdZ
-            self.dZs.append(curDZ)
+        for l in range(self.L-2, 1, -1):
+            sigmaPrime = self.ReLUPrime if self.activations[l] ==\
+                "ReLU" else self.sigmoidPrime
+            self.deltas[l] = (self.Ws[l+1].T @ self.deltas[l+1]) *\
+                sigmaPrime(self.Zs[l])
