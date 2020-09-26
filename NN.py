36dbcff22c3534328e8b0f53753bd9b2644fdf66
Marc Harary <marcharary@vpn172022158245.its.yale.internal>
2020-09-26 17:28:20 -0400

    Simplified activation functions and derivatives.

--- NN.py
+++ NN.py
@@ -5,22 +5,21 @@ import numpy as np
 class NN:
     """ implements fully-connected feed-forward neural network """
 
-    def __init__(self, layers, activations):
+    def __init__(self, ns, acts):
         # check for matching input dimensions
-        assert(len(layers) >= 2)
-        assert(all(map(lambda x: x == "ReLU" or x == "sigmoid",
-                       activations)))
-        assert(len(layers) == len(activations))
+        assert(len(ns) >= 2)
+        assert(all(map(lambda x: x == "ReLU" or x == "sigmoid",acts)))
+        assert(len(ns) == len(acts))
 
-        self.activations = activations
-        self.L = len(layers)
+        self.acts = acts
+        self.L = len(ns)
 
         # zero initialization for bias vectors
-        self.bs = [np.zeros((layer,1)) for layer in layers]
+        self.bs = [np.zeros((n,1)) for n in ns]
         # Xavier initialization for weight vectors
         self.Ws = []
-        for i in range(1,len(layers)):
-            shape = (layers[i-1], layers[i])
+        for i in range(1,len(ns)):
+            shape = (ns[i-1], ns[i])
             self.Ws.append(.01*np.random.randn(*shape))
 
     
@@ -40,25 +39,16 @@ class NN:
             truths """
         pass
 
-
-    def ReLUPrime(self, z):
-        """ derivative of ReLU activation function """
-        return 0 if z < 0 else 1
-
-
-    def sigmoidPrime(self, z):
-        """ derivative of sigmoid activation function """
-        return self.sigmoid(z)*(1-self.sigmoid(z))
+    def gPrime(self, func, z):
+        if func == "ReLU":
+            return 0 if z < 0 else 1
+        return self.g("sigmoid",z) * (1-self.g("sigmoid",z))
 
 
-    def sigmoid(self, z):
-        """ sigmoid activation function """
+    def g(self, func, z):
+        if func == "ReLU":
+            return np.clip(z, a_min=0, a_max=None)
         return (1+np.exp(-z))**-1
-
-
-    def ReLU(self, z):
-        """ ReLU activation function """
-        return np.clip(z, a_min=0, a_max=None)
 
 
     def forward_propagate(self, X):
@@ -69,44 +59,32 @@ class NN:
 
         # calculate activation for first layer
         Z1 = self.Ws[0].T @ X
-        if self.activations[0] == "ReLU":
-            A1 = self.ReLU(Z1)
-        else:
-            A1 = self.sigmoid(Z1)
+        A1 = self.g(self.acts[0], Z1)
         self.Zs.append(Z1)
         self.As.append(A1)
         
-        # calculate activations for subsequent layers
+        # calculate acts for subsequent layers
         for i in range(1, len(self.Ws)):
             Z = self.Ws[i].T @ self.As[i] + self.bs[i]
-            if self.activations[i] == "ReLU":
-                A = self.ReLU(Z)
-            else:
-                A = self.sigmoid(Z)
+            A = self.g(self.acts[i], Z)
             self.Zs.append(Z)
             self.As.append(A)
+
+        return self.As[-1]
 
     
     def back_propagate(self, Y):
         """ gets gradients for each layers given forward propagation
             has already occurred """
-        m = self.Ws[0].shape[1]
-        
-        self.deltas = self.dAs = self.dWs = self.dbs = self.L * [None]
+        self.deltas = self.dWs = self.dbs = self.L * [None]
     
-        # calculate backprop for last layer
-        self.dAs[self.L-1] = self.As[self.L-1] - Y
-        if self.activations[self.L-1] == "ReLU":
-            # apply proper g'(.)
-            self.deltas[self.L-1] = self.dAs[self.L-1] *\
-                self.ReLUPrime(self.Zs[self.L-1])
-        else:
-            self.deltas[self.L-1] = self.dAs[self.L-1] *\
-                self.sigmoidPrime(self.Zs[self.L-1])
+        # calculate delta for last layer
+        dA = self.As[self.L] - Y # dC/dAl
+        self.deltas[self.L] = dA * self.gPrime(self.acts[self.L],
+                                               self.Zs[self.L])
 
         # calculate backprop for subsequent layers
         for l in range(self.L-2, 1, -1):
-            sigmaPrime = self.ReLUPrime if self.activations[l] ==\
-                "ReLU" else self.sigmoidPrime
-            self.deltas[l] = (self.Ws[l+1].T @ self.deltas[l+1]) *\
-                sigmaPrime(self.Zs[l])
+            # get proper deriative function
+            self.deltas[l] = self.Ws[l].T @ self.deltas[l+1] *\
+                self.gPrime(self.acts[l], self.Zs[l])
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from NN import NN
 import numpy as np
 
 def main():
-    nn = NN(layers=[5,3,2,1],
-            activations=["ReLU", "ReLU", "ReLU", "sigmoid"])
-    X = np.random.random((5,2))
-    nn.forward_propagate(X)
-    print(nn.As[-1])
+    nn = NN(ns=[5,5,1], acts=["ReLU", "ReLU", "sigmoid"])
+    X = np.random.random((5,1))
+    # print(nn.L)
+    output = nn.forward_propagate(X)
+    print(output)
+    # nn.back_propagate(1)
 
 if __name__ == "__main__":
     main()
