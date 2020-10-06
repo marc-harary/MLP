3387366cadca21b376c54ed71978e09413be8cac
Marc Harary <marcharary@Marcs-MacBook-Air.local>
2020-10-06 18:07:40 -0400

    Simplified fit method in NN.py and created main test script

--- NN.py
+++ NN.py
@@ -11,8 +11,9 @@ class NN:
         assert(all(map(lambda act: act in ["ReLU","sigmoid"], acts)))
         assert(len(ns) == len(acts))
 
-        self.L = len(ns)
-        self.acts = acts
+        self.ns     = ns
+        self.L      = len(ns)
+        self.acts   = acts
         self.deltas = self.L * [None]
         self.dWs    = self.L * [None]
         self.dbs    = self.L * [None]
@@ -20,17 +21,19 @@ class NN:
         self.Zs     = self.L * [None]
         self.bs     = self.L * [None] # 0 initialization
         self.Ws     = self.L * [None] # Xavier initialization
+        self.output = None
         for i in range(1, self.L):
-            self.Ws[i] = .01 * np.random.randn(ns[i-1], ns[i])
+            self.Ws[i] = .01 * np.random.randn(ns[i], ns[i-1])
             self.bs[i] = np.zeros((ns[i],1))
         
 
-    def cross_entropy_loss(self, Y, lambd):
+    def cross_entropy_loss(self, X, Y, lambd):
         """ returns cross-entropy loss with regularization """
         m = self.Ws[1].shape[1]
-        A = self.As[-1]
+        A = self.forward_propagate(X)
         loss = -np.mean(Y*np.log(A) + (1-Y)*np.log(1-A)) # w/out reg
-        regular = [np.square(W).sum() for W in self.Ws] # reg term
+        regular = [np.square(W).sum() for W in self.Ws
+                   if W is not None]
         regular = lambd/(2*m) * sum(regular)
         return loss + regular
 
@@ -38,7 +41,8 @@ class NN:
     def evaluate(self, X, Y):
         """ evaluates F1 score of model given inputs and ground
             truths """
-        pass
+        A = self.forward_propagate(X) > .5
+        return np.mean(A == Y)
 
 
     def gPrime(self, act, z):
@@ -56,22 +60,22 @@ class NN:
         if act == "ReLU":
             return np.clip(z, a_min=0, a_max=None)
         elif act == "sigmoid":
-            return (1+np.exp(-z))**-1
+            return 1/(1+np.exp(-z))
 
 
     def forward_propagate(self, X):
         """ propagates tensor forwards through network """
         # check dimensions of input matrix match those of input layer
-        assert(X.shape[0] == self.Ws[1].shape[0])
+        assert(X.shape[0] == self.Ws[1].shape[1])
 
         # forward propagate and cache entire batch
         self.As[0] = X
         for i in range(1, self.L):
-            self.Zs[i] = self.Ws[i].T @ self.As[i-1] + self.bs[i]
+            self.Zs[i] = self.Ws[i] @ self.As[i-1] + self.bs[i]
             self.As[i] = self.g(self.acts[i], self.Zs[i])
 
         # cache output for entire batch
-        output = self.As[-1]
+        self.output = self.As[-1]
 
         # re-store average activations and weighted inputs
         self.As[0] = np.mean(X, axis=1, keepdims=True)
@@ -79,34 +83,47 @@ class NN:
             self.As[i] = np.mean(self.As[i], axis=1, keepdims=True)
             self.Zs[i] = np.mean(self.Zs[i], axis=1, keepdims=True)
 
-        return output
+        return self.output
 
 
     def back_propagate(self, Y):
         """ gets gradients for each layers given forward propagation
             has already occurred """
         # delta for last layer
-        dA = np.mean(self.As[-1] - Y, axis=1, keepdims=True) 
-        self.deltas[-1] = dA * self.gPrime(self.acts[-1], self.Zs[-1])
-        self.dWs[-1] = self.deltas[-1] @ self.As[-2].T
-        
-        # deltas for preceding layers
-        for l in range(self.L-2, 0, -1): 
-            self.deltas[l] = self.Ws[l+1] @ self.deltas[l+1]
-            self.deltas[l] *= self.gPrime(self.acts[l], self.Zs[l])
-            self.dWs[l] = self.deltas[l] @ self.As[l-1].T
-            self.dbs[l]=np.sum(self.deltas[l+1],axis=1,keepdims=True)
+        self.deltas[-1] = np.mean(self.output-Y, axis=1,
+                                  keepdims=True) *\
+                            self.gPrime(self.acts[-1], self.Zs[-1])
 
-    
-    def fit(self, X, Y, epochs=10_000, alpha=1e-2):
+        # deltas for preceding layers
+        for l in range(self.L-2, 0, -1):
+            self.deltas[l] = self.Ws[l+1].T @ self.deltas[l+1] *\
+                self.gPrime(self.acts[l], self.Zs[l])
+
+        # calculate dbs and dWs
+        for l in range(self.L-1, 0, -1):
+            self.dbs[l] = self.deltas[l]
+            self.dWs[l] = self.deltas[l] @ self.As[l-1].T
+
+
+    def fit(self,
+            X,
+            Y,
+            epochs=10_000,
+            alpha=1e-2,
+            printProg=False,
+            lambd=1e-2
+        ):
         for epoch in range(epochs):
+            # get all activations, weighted inputs, and gradients
             self.forward_propagate(X)
             self.back_propagate(Y)
 
+            # gradient descent update
             for l in range(1, self.L):
                 self.Ws[l] -= alpha*self.dWs[l]
                 self.bs[l] -= alpha*self.bs[l]
 
-            loss = self.cross_entropy_loss(Y, lambd=0)
-            if epoch % 1000 == 0:
-                print("Loss for epoch {epoch} = {loss}"%(epoch,loss))
+            # calculate loss
+            loss = self.cross_entropy_loss(X, Y, lambd=lambd)
+            if epoch % 1_000 == 0 and printProg:
+                print(f"Loss for epoch {epoch} = {loss}")
--- main.py
+++ main.py
@@ -1,13 +1,65 @@
-from NN import NN
 import numpy as np
+import pandas as pd
+from NN import NN
+from perceptron import Perceptron
+from sklearn.linear_model import Perceptron as standardPerceptron
+
 
 def main():
-    nn = NN(ns=[5,5,1], acts=["ReLU", "ReLU", "sigmoid"])
-    X = np.random.random((5,1))
-    # print(nn.L)
-    output = nn.forward_propagate(X)
-    print(output)
-    # nn.back_propagate(1)
+    # load data into shuffle matrix
+    df = pd.read_csv("diabetes.csv")
+    data = np.array(df)
+    np.random.shuffle(data)
+
+    # split data into labels and design matrix
+    design = data[:, :-1].T
+    labels = data[:, -1]
+    labels = labels.reshape((labels.shape[0],1)).T # keep dims to 2
+    n, m_tot = design.shape
+
+    # normalize dataset
+    maxs = np.amax(design, axis=1)
+    mins = np.amin(design, axis=1)
+    ranges = maxs - mins
+    design -= mins.reshape(len(mins),1)
+    design /= ranges.reshape(len(ranges),1)
+
+    # split into test and training data
+    frac_test = .8
+    split_idx = int(frac_test*m_tot)
+    train_design = design[:, :split_idx]
+    train_labels = labels[:, :split_idx]
+    test_design = design[:, split_idx:]
+    test_labels = labels[:, split_idx:]
+
+    # fit neural network
+    nn = NN(ns=[n,5,1], acts=["ReLU","ReLU","sigmoid"])
+    nn.fit(train_design, train_labels, alpha=1e-2, epochs = 20_000)
+    test_acc = nn.evaluate(X=test_design, Y=test_labels)
+    train_acc = nn.evaluate(X=train_design, Y=train_labels)
+    print("Network test set accuracy: %.5f" % test_acc)
+    print("Network training set accuracy: %.5f" % train_acc)
+    print()
+
+    # fit perceptron
+    perc = Perceptron()
+    perc.fit(X=train_design, Y=train_labels, alpha=1e-4,
+        lambd=1e-2, epochs=100_000)
+    test_acc = perc.acc(X=test_design, Y=test_labels)
+    train_acc = perc.acc(X=train_design, Y=train_labels)
+    print("Own perceptron test set accuracy: %.5f" % test_acc)
+    print("Own perceptron training set accuracy: %.5f" % train_acc)
+    print()
+
+    # fit standard template perceptron from sklearn
+    clf = standardPerceptron(tol=1e-3, random_state=0)
+    clf.fit(train_design.T, train_labels.squeeze())
+    train_acc = clf.score(train_design.T, train_labels.squeeze())
+    test_acc = clf.score(test_design.T, test_labels.squeeze())
+    print("Sklearn perceptron test set accuracy: %.5f" % test_acc)
+    print("Sklearn perceptron training set accuracy: %.5f"%train_acc)
+    print()
+
 
 if __name__ == "__main__":
     main()
